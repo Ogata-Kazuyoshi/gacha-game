b369582f98e2bf16117d5d1d3dcd25d2413817d1
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-05-03 23:11:59 +0900

    sound追加

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import useSound from "use-sound";
+import Sound from "../assets/sound/gatyagatya.mp3"
+
 export const Home = () => {
-    return <>Home</>
+    const [play, { stop, pause }] = useSound(Sound);
+
+    return (
+        <>
+            <button onClick={() => play()}>音を鳴らす</button>
+            <button onClick={() => stop()}>停止</button>
+            <button onClick={() => pause()}>ポーズ</button>
+        </>
+    );
 }
