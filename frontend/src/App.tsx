89bac322b4ea657686d498b69125c8e756814872
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-05-03 22:01:18 +0900

    temp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import {AppCommon} from "./pages/AppCommon.tsx";
 import {Home} from "./pages/Home.tsx";
+import {Temp} from "./pages/Temp.tsx";
 
 function App() {
 
   return (
     <>
       <Routes>
+          <Route path="/" element={<Temp/>}></Route>
           <Route path="app" element={<AppCommon/>}>
               <Route path="home" element={<Home/>}/>
           </Route>
