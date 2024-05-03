0e7f6d5791c3a579f991320e9271397bcc4c410c
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-05-03 22:36:19 +0900

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import {InlineConfig} from "vitest";
+import {VitePWA} from "vite-plugin-pwa";
 
 const testConfig: InlineConfig = {
   environment: 'jsdom',
@@ -12,5 +13,50 @@ const testConfig: InlineConfig = {
 // https://vitejs.dev/config/
 export default defineConfig({
   test: testConfig,
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      strategies:'generateSW',
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: null,
+      // includeAssets: ['apple-touch-icon.png'],
+      manifest: {
+        "theme_color": " #000000",
+        "background_color": "#ffffff",
+        "display": "standalone",
+        "orientation":"any",
+        "start_url": "/",
+        "name": "temp",
+        "short_name": "temp",
+        // "icons": [
+        //   {
+        //     "src": "/Icon192.png",
+        //     "sizes": "192x192",
+        //     "type": "image/png"
+        //   },
+        //   {
+        //     "src": "/Icon256.png",
+        //     "sizes": "256x256",
+        //     "type": "image/png"
+        //   },
+        //   {
+        //     "src": "/Icon384.png",
+        //     "sizes": "384x384",
+        //     "type": "image/png"
+        //   },
+        //   {
+        //     "src": "/Icon512.png",
+        //     "sizes": "512x512",
+        //     "type": "image/png",
+        //   }
+        // ]
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/api/,/^\/login/,/^\/oauth2/],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    }),
+  ],
 })
