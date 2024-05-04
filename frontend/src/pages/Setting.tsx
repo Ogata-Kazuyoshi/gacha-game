284649dfed4cf6d79a2676b4f55bacd9a0fc6388
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-05-04 23:21:42 +0900

    ローカルストレージのデータをけす機能実装

--- frontend/src/pages/Setting.tsx
+++ frontend/src/pages/Setting.tsx
@@ -1,3 +1,16 @@
+import data from "../data/data.json"
+
 export const Setting = () => {
-    return <>setting</>
+
+    const removeLocalstorage = () => {
+        data.dataList.forEach((elm)=>{
+            window.localStorage.removeItem(`gachaList-${elm.titleNumber}`)
+        })
+    }
+
+    return <>
+    <div>
+        <button onClick={removeLocalstorage}>でーたをけす</button>
+    </div>
+    </>
 }
